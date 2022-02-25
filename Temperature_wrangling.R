9e64febfb9697ec42a6b7ab433c9b24b6f8af594
s1735858 <s1735858@ed.ac.uk>
2022-02-25 14:43:01 +0000

    Colombe calculates monthly T averages for MZB

--- Temperature_wrangling.R
+++ Temperature_wrangling.R
@@ -13,7 +13,7 @@ data <- read.csv("MZB_raw_T_data.csv")
 
 ###### CHECK AMOUNT OF NA DATA BEFORE NA.OMIT -- esp MZB 2009, 2010
 
-# Wrangle
+# Separating date/time, creating month
 MZB <- data %>% 
   separate(Date.Time_UTC, c("Date", "Time"), sep = " ", remove = TRUE) %>%
   select(-Time) %>% 
@@ -29,17 +29,36 @@ MZB <- data %>%
     grepl("09/", Date) ~ "September",
     grepl("10/", Date) ~ "October",
     grepl("11/", Date) ~ "November",
-    grepl("12/", Date) ~ "December")) 
-group_by(Month) %>% 
-  mutate(Mean_temp = mean(Temp))
+    grepl("12/", Date) ~ "December"))
 
 MZB$Temp <- as.numeric(MZB$Temp)
-MZB2 <- na.omit(MZB)
 
-MZB_mean <- MZB2 %>%
-  mutate(Date = case_when(
+# Calculating monthly averages 2000-2020
+MZB_mean <- MZB %>%
+  na.omit(MZB) %>% 
+  mutate(Year = case_when(
     grepl("2000", Date) ~ "2000",
-    grepl("2001", Date) ~ "2001")) %>% 
-  group_by(Site, Month, Date) %>% 
-  summarise(Mean_temp = mean(Temp))
-
+    grepl("2001", Date) ~ "2001",
+    grepl("2002", Date) ~ "2002",
+    grepl("2003", Date) ~ "2003",
+    grepl("2004", Date) ~ "2004",
+    grepl("2005", Date) ~ "2005",
+    grepl("2006", Date) ~ "2006",
+    grepl("2007", Date) ~ "2007",
+    grepl("2008", Date) ~ "2008",
+    grepl("2009", Date) ~ "2009",
+    grepl("2010", Date) ~ "2010",
+    grepl("2011", Date) ~ "2011",
+    grepl("2012", Date) ~ "2012",
+    grepl("2013", Date) ~ "2013",
+    grepl("2014", Date) ~ "2014",
+    grepl("2015", Date) ~ "2015",
+    grepl("2016", Date) ~ "2016",
+    grepl("2017", Date) ~ "2017",
+    grepl("2018", Date) ~ "2018",
+    grepl("2019", Date) ~ "2019",
+    grepl("2020", Date) ~ "2020")) %>%
+  select(-Date) %>% 
+  group_by(Site, Month, Year) %>% 
+  summarise(Mean_temp = mean(Temp)) %>% 
+  ungroup()
