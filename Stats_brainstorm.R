1c2610f8e8885334cc5ec921eba961164bdd5c0b
s1735858 <s1735858@ed.ac.uk>
2022-03-29 16:33:19 +0100

    Colombe adds KGI99 and brainstroms binomial GLM

--- Stats_brainstorm.R
+++ Stats_brainstorm.R
@@ -10,6 +10,7 @@ library(ggplot2)
 library(lme4)
 library(stargazer)
 library(MuMIn)
+library(corrplot)
 
 # Importing data ----
 data <- read.csv("Tidy_summer_data.csv")
@@ -17,23 +18,27 @@ data <- read.csv("Tidy_summer_data.csv")
 # Transforming data ----
 # logging data
 data <- na.omit(data)
-data$Region <- as.factor(as.character(data$Region))
+data <- mutate(data, Cover_log2 = log(Percentage_cover + 1))
+
 data_west <- filter(data, Region == "West")
 data_east <- filter(data, Region == "East")
 
-data <- mutate(data, Cover_log = log(Percentage_cover))
-hist(data$Cover_log)
-(log_hist <- ggplot(data = data, aes(x = Cover_log)) +
-      geom_histogram(binwidth = 1))
+scaled_data <- data %>% 
+   mutate(scaled_cover = scale(data$Percentage_cover, center = TRUE, scale = TRUE),
+          scaled_log = scale(data$Cover_log2, center = TRUE, scale = TRUE),
+          proportion_cover = Percentage_cover/100, 
+          site_nb = case_when(
+             grepl("Cape_Hallet", Site) ~ "1",
+             grepl("Casey", Site) ~ "2",
+             grepl("MZB", Site) ~ "3",
+             grepl("KGI", Site) ~ "4",
+             grepl("Anch_Isl", Site) ~ "5",
+             grepl("Lag_Isl", Site) ~ "6"
+          )) 
 
-data <- mutate(data, Cover_log2 = log(Percentage_cover + 1))
 hist(data$Cover_log2)
 (log_hist2 <- ggplot(data = data, aes(x = Cover_log2)) +
       geom_histogram(binwidth = 1))
-
-# square root
-data <- mutate(data, Cover_sqrt = sqrt(Percentage_cover))
-hist(data$Cover_sqrt)
 
 # averaging T data
 t_mean <- data %>% 
@@ -67,7 +72,14 @@ t_mean <- data %>%
 
 # model
 temp_time <- lm(Mean_temp ~ Year + Region, data = data)
-summary(temp_time) # significant (overall p + West), Adj-R2 = 0.547
+summary(temp_time) # significant (overall p + West), Adj-R2 = 0.655
+
+temp_time_resids <- resid(temp_time)
+shapiro.test(temp_time_resids) # NORMALLY DISTRIBUTED
+bartlett.test(Mean_temp ~ Year + Region, data = data)
+bartlett.test(temp_time)
+par(mfrow=c(1,1))
+plot(temp_time)
 
 # per region
 # west
@@ -76,7 +88,7 @@ hist(data_west$Mean_temp)
    geom_point() +
    geom_smooth(method = "lm"))
 west_temp <- lm(Mean_temp ~ Year, data = data_west)
-summary(west_temp)
+summary(west_temp) # not significant, adj-R2 = 0.004
 
 # east
 hist(data_east$Mean_temp)
@@ -84,7 +96,7 @@ hist(data_east$Mean_temp)
       geom_point() +
       geom_smooth(method = "lm"))
 east_temp <- lm(Mean_temp ~ Year, data = data_east)
-summary(east_temp)
+summary(east_temp) # not significant, adj-R2 = -0.052
 
 # Mean T data ----
 # Plotting data
@@ -102,7 +114,7 @@ summary(Mean_t_ancova) # significant (overall + West), adj-R2 = 0.7707
 
 # RQ1: HOW DOES VEGETATION COVER DIFFER BETWEEN THE 2 REGIONS? ----
 # could average out present vegetaion cover in each region do box plot??
-
+# cancelling this RQ
 
 
 # RQ2: VEGETATION COVER CHANGE OVER TIME ----
@@ -121,9 +133,19 @@ hist(veg_resids)
 shapiro.test(veg_resids) # not normally distributed
 
 # LINEAR MIXED MODEL
+# Model selection
 veg_mixed_interaction <- lmer(Cover_log2 ~ Year*Region + (1|Site), data = data, REML = FALSE)
-summary(veg_mixed_interaction) # Site explains ~54% of leftover variance after the variance explained by fixed effect
+summary(veg_mixed_interaction) # Site explains ~54% of leftover variance after the variance explained by fixed effect (without KGI99)
+# Site expains 0% residual variation with KGI99
 
+data <- filter(data, Year != "1999") # substracting KGI99
+
+veg_scaled_interaction <- lmer(scaled_cover ~ Year*Region + (1|Site), data = scaled_data, REML = FALSE)
+summary(veg_scaled_interaction) # Site explains 21.3% residual variation with scaled_cover
+veg_scaled_log_interaction <- lmer(scaled_log ~ Year*Region + (1|Site), data = scaled_data, REML = FALSE)
+summary(veg_scaled_log_interaction) # Site explains 0% variation with scaled_log
+
+# Model comparison: non-scaled data
 veg_mixed_null <- lmer(Cover_log2 ~ 1 + (1|Site), data = data, REML = FALSE)
 veg_mixed <- lmer(Cover_log2 ~ Year + Region + (1|Site), data = data, REML = FALSE)
 veg_mixed_simple <- lm(Cover_log2 ~ Year*Region, data = data, REML = FALSE)
@@ -132,10 +154,22 @@ veg_mixed_random <- lmer(Cover_log2 ~ Year*Region + (Region|Site), data = data, 
 
 AIC(veg_mixed, veg_mixed_interaction, veg_mixed_null, veg_mixed_simple, veg_mixed_crossed, veg_mixed_random)
 AICc(veg_mixed, veg_mixed_interaction, veg_mixed_null, veg_mixed_simple, veg_mixed_crossed, veg_mixed_random)
+
 # should use intercation as there's barely any difference between null and no interaction
 # AIC of random slope model is way higher than all the others when using interaction
 # HOWEVER, AIC of random slope is the lowest when only using Region
 # AIC and AICc of intercation & random effect is lower than of interaction w/o random effect and of interaction w/ nested
+
+# Model comparison: scaled-data
+veg_scaled_null_s <- lmer(scaled_cover ~ 1 + (1|Site), data = scaled_data, REML = FALSE)
+veg_scaled_null_t <- lmer(scaled_cover ~ 1 + (1|Year), data = scaled_data, REML = FALSE)
+veg_scaled_null_t_s <- lmer(scaled_cover ~ 1 + (1|Year) + (1|Site), data = scaled_data, REML = FALSE)
+veg_scaled_simple_interaction <- lm(scaled_cover ~ Year*Region, data = scaled_data, REML = FALSE)
+veg_scaled_simple <- lm(scaled_cover ~ Year +Region, data = scaled_data, REML = FALSE)
+summary(veg_scaled_simple_interaction)
+AIC(veg_scaled_interaction, veg_scaled_null_s, veg_scaled_null_t, veg_scaled_null_t_s, veg_scaled_simple_interaction, veg_scaled_simple)
+AICc(veg_scaled_interaction, veg_scaled_null_s, veg_scaled_null_t, veg_scaled_null_t_s, veg_scaled_simple_interaction, veg_scaled_simple)
+
 
 r.squaredGLMM(veg_mixed_interaction) # R2m = 0.306; R2c = 0.685, i.e. including random effect increases explanatory power
 r.squaredGLMM(veg_mixed_null)
@@ -155,13 +189,23 @@ stargazer(veg_mixed_interaction, type = "text",
 plot(veg_mixed_interaction) # resid vs fitted
 
 qqnorm(resid(veg_mixed_interaction))
-qqline(resid(veg_mixed_interaction)) # really good normal Q-Q plot
+qqline(resid(veg_mixed_interaction)) # good normal Q-Q plot
 
 plot(veg_mixed_interaction,
      sqrt(abs(resid(.)))~fitted(.),
      type=c("p","smooth"), col.line=1) # scale-location not so good
 
 plot(veg_mixed_interaction, rstudent(.) ~ hatvalues(.)) # resids vs leverage
+
+bartlett.test(veg_mixed_interaction)
+
+# correlation analysis
+data_correlation_w <- data_west %>% 
+   select(-Region) %>% 
+   select(-Site) %>% 
+   select(-Year)
+
+corrplot(cor(data_correlation_w))
 
 # log linear model
 veg_log <- lm(Cover_log ~ Year, data = data)
@@ -181,14 +225,14 @@ shapiro.test(interaction_resids) # not normally distributed
 # log-ancova: time and region
 # without interaction
 veg_ancova_log <- lm(Cover_log2 ~ Year + Region, data = data)
-summary(veg_ancova_log) # significant, adj-R2 = 0.119
+summary(veg_ancova_log) # significant, adj-R2 = 0.113
 
 # with interaction
 veg_ancova_log_interaction <- lm(Cover_log2 ~ Year*Region, data = data)
-summary(veg_ancova_log_interaction) # highly significant, adj-R2 = 0.453
+summary(veg_ancova_log_interaction) # highly significant, adj-R2 = 0.357
 veg_interaction_resids <- resid(veg_ancova_log_interaction)
-shapiro.test(veg_interaction_resids) # not.... NORMALLY DISTRIBUTED BABY
-bartlett.test(Cover_log2 ~ Year*Region, data = data)
+shapiro.test(veg_interaction_resids) # NORMALLY DISTRIBUTED
+bartlett.test(veg_mixed_interaction)
 plot(veg_ancova_log_interaction)
 
 # log-ancova: time and site
@@ -202,9 +246,60 @@ summary(veg_site_log_interaction) # highly significant overall, adj-R2 = 0.624
 veg_site_log_resids <- resid(veg_site_log_interaction)
 shapiro.test(veg_site_log_resids) # normally distributed
 
+# GENERALISED LINEAR MODEL
+# Model comparison
+veg_glm_simple <- glm(Percentage_cover ~ Year + Region, data = scaled_data, family = binomial)
+veg_glm_scaled_simple <- glm(scaled_cover ~ Year + Region, data = scaled_data)
+veg_glm_proport_simple <- glm(proportion_cover ~ Year + Region, data = scaled_data, family = binomial)
+summary(veg_glm_proport_simple)
+veg_glm_scaled_interaction <- glm(scaled_cover ~ Year*Region, data = scaled_data, family = binomial)
+summary(veg_glm_scaled_interaction)
+veg_glm_scaled_null <- glm(scaled_cover ~ 1, data = scaled_data, family = binomial)
+veg_glm_scaled_time <- glm(scaled_cover ~ Year, data = scaled_data)
+veg_glm_scaled_region <- glm(scaled_cover ~ Region, data = scaled_data)
+veg_glm_scaled_interaction_s <- glmer(scaled_cover ~ Year*Region + (1|Site), data = scaled_data)
+veg_glm_scaled_interaction_t <- glmer(scaled_cover ~ Year*Region + (1|Year), data = scaled_data)
+veg_glm_scaled_interaction_t_s <- glmer(scaled_cover ~ Year*Region + (1|Year) + (1|Site), data = scaled_data)
+
+AIC(veg_glm_simple, veg_glm_scaled_simple, veg_glm_scaled_interaction, veg_glm_scaled_null, veg_glm_scaled_time, veg_glm_scaled_region, veg_glm_scaled_interaction_t, veg_glm_scaled_interaction_s, veg_glm_scaled_interaction_t_s)
+AICc(veg_glm_simple, veg_glm_scaled_simple, veg_glm_scaled_interaction, veg_glm_scaled_null, veg_glm_scaled_time, veg_glm_scaled_region, veg_glm_scaled_interaction_t, veg_glm_scaled_interaction_s, veg_glm_scaled_interaction_t_s)
+
+# AICc for simple model with normal data is 415 and for scaled data is 139, i.e. should use scaled data
+# AICc for scaled model without intercation is 139 and for scaled with interaction is 136, i.e. should use interaction
+# AICc of null model equivalent to interaction model, i.e. no influence of Region and Year...
+
+# Model comparison: proportion data
+veg_glm_proport_simple <- glm(proportion_cover ~ Year + Region, data = scaled_data, family = binomial)
+veg_glm_proport_interaction <- glm(proportion_cover ~ Year*Region, data = scaled_data, family = binomial)
+veg_glm_proport_null <- glm(proportion_cover ~ 1, data = scaled_data, family = binomial)
+veg_glm_proport_t <- glmer(proportion_cover ~ Year + Region + (1|Year), data = scaled_data, family = binomial, nAGQ = 0)
+veg_glm_proport_s <- glmer(proportion_cover ~ Year + Region + (1|Site), data = scaled_data, family = binomial)
+veg_glm_proport_s_interaction <- glmer(proportion_cover ~ Year*Region + (1|Site), data = scaled_data, family = binomial)
+veg_glm_proport_s_t <- glmer(proportion_cover ~ Year + Region + (1|Site) + (1|Year), data = scaled_data, family = binomial)
+veg_glm_proport_s_t_interaction <- glmer(proportion_cover ~ Year*Region + (1|Site) + (1|Year), data = scaled_data, family = binomial, nAGQ = 0)
+veg_glm_proport_s_random <- glmer(proportion_cover ~ Year + Region + (Region|Site), data = scaled_data, family = binomial)
+
+AIC(veg_glm_proport_simple, veg_glm_proport_interaction, veg_glm_proport_null, veg_glm_proport_s, veg_glm_proport_s_t, veg_glm_proport_t, veg_glm_proport_s_t_interaction)
+AICc(veg_glm_proport_simple, veg_glm_proport_interaction, veg_glm_proport_null, veg_glm_proport_s, veg_glm_proport_s_t, veg_glm_proport_t, veg_glm_proport_s_t_interaction, veg_glm_proport_s_random)
+
+summary(veg_glm_proport_s)
+stargazer(veg_glm_proport_s, type = "text",
+          digits = 3,
+          star.cutoffs = c(0.05, 0.01, 0.001),
+          digit.separator = "")
+
+# AICc for interaction is higher (32) than without interaction (28), i.e. shouldn't use interaction
+# try nAGQ = 0 to fix non-convergence in Year model
+
+# checking validation 
+plot(veg_glm_proport_s)
+plot(proportion_cover ~ Year + Region + (1|Site), data = scaled_data, family = binomial)
+points(scaled_data$Year, fitted(veg_glm_proport_s))
+
 # Plotting data ----
+# LOOK INTO ggeffects
 # plotting data per site
-(veg_scatter <- ggplot(data, aes(x = Year, y = Percentage_cover)) +
+(veg_scatter <- ggplot(data, aes(x = Year, y = Cover_log2)) +
    geom_point() +
    geom_smooth(method = "lm") +
    facet_wrap(~Site, scales = "free_y") +
@@ -234,23 +329,25 @@ shapiro.test(veg_site_log_resids) # normally distributed
 # ancova
 # without interaction
 veg_temp <- lm(Percentage_cover ~ Mean_temp + Region, data = data)
-summary(veg_temp) # not significant, adj-R2 = -0.015
+summary(veg_temp) # not significant, adj-R2 = 0.011
 
 # with intercation
 veg_temp_interaction <- lm(Percentage_cover ~ Mean_temp*Region, data = data)
-summary(veg_temp_interaction) # not significant overall (p = 0.051), interaction term significant, adj-R2 = 0.107
+summary(veg_temp_interaction) # not significant overall, interaction term significant, adj-R2 = -0.003
 
 # log-ancova: temp and region
 # without interaction
 veg_temp_log <- lm(Cover_log2 ~ Mean_temp + Region, data = data)
-summary(veg_temp_log) # significant, adj-R2 = 0.1
-shapiro.test(veg_temp_log)
+summary(veg_temp_log) # significant (overall p + East), adj-R2 = 0.1028
+shapiro.test(veg_temp_log) # doesn't work
+bartlett.test(Cover_log2 ~ Mean_temp + Region, data = data)
+plot(veg_temp_log)
 
 # with interaction
 veg_temp_log_interaction <- lm(Cover_log2 ~ Mean_temp*Region, data = data)
-summary(veg_temp_log_interaction) # significant, adj-R2 = 0.138
+summary(veg_temp_log_interaction) # not significant, adj-R2 = 0.086
 veg_temp_interaction_resids <- resid(veg_temp_log_interaction)
-shapiro.test(veg_temp_interaction_resids) # NORMALLY DISTRIBUTED
+shapiro.test(veg_temp_interaction_resids) # NORMALLY DISTRIBUTED (not if include KGI99)
 bartlett.test(Cover_log2 ~ Mean_temp*Region, data = data)
 par(mfrow=c(1,1))
 plot(veg_temp_log_interaction)
@@ -277,11 +374,38 @@ summary(veg_temp_sqrt_interaction) # significant, adj-R2 = 0.119
 
 # LINEAR MIXED MODEL
 veg_temp_mixed_interaction <- lmer(Cover_log2 ~ Mean_temp*Region + (1|Site), data = data, REML = FALSE)
-summary(veg_temp_mixed_interaction) # Site explains ~54% of leftover variance after the variance explained by fixed effect
+summary(veg_temp_mixed_interaction) # LMM doesn't explain more if include KGI99
 
 veg_temp_mixed <- lmer(Cover_log2 ~ Mean_temp + Region + (1|Site), data = data, REML = FALSE)
 summary(veg_temp_mixed)
 
+# GENERALISED LINEAR MODEL
+# Model comparison: scaled
+
+veg_temp_glm_simple <- glm(Percentage_cover ~ Mean_temp + Region, data = scaled_data)
+veg_temp_glm_scaled_simple <- glm(scaled_cover ~ Mean_temp + Region, data = scaled_data)
+veg_temp_glm_scaled_interaction <- glm(scaled_cover ~ Mean_temp*Region, data = scaled_data)
+veg_temp_glm_scaled_null <- glm(scaled_cover ~ 1, data = scaled_data)
+
+AIC(veg_temp_glm_simple, veg_temp_glm_scaled_simple, veg_temp_glm_scaled_interaction)
+AICc(veg_temp_glm_simple, veg_temp_glm_scaled_simple, veg_temp_glm_scaled_interaction)
+
+# AICc of scaled much lower (141) than non scaled (415), i.e. should use scaled data
+
+# Model comparison: proportion data
+veg_temp_glm_proport_simple <- glm(proportion_cover ~ Mean_temp + Region, data = scaled_data, family = binomial)
+veg_temp_glm_proport_interaction <- glm(proportion_cover ~ Mean_temp * Region, data = scaled_data, family = binomial)
+veg_temp_glm_proport_null <- glm(proportion_cover ~ 1, data = scaled_data, family = binomial)
+veg_temp_glm_proport_s <- glmer(proportion_cover ~ Mean_temp + Region + (1|Site), data = scaled_data, family = binomial)
+veg_temp_glm_proport_s_random <- glmer(proportion_cover ~ Mean_temp + Region + (Region|Site), data = scaled_data, family = binomial)
+veg_temp_glm_proport_s_interaction <- glmer(proportion_cover ~ Mean_temp * Region + (1|Site), data = scaled_data, family = binomial)
+veg_temp_glm_proport_t <- glmer(proportion_cover ~ Mean_temp + Region + (1|Year), data = scaled_data, family = binomial)
+veg_temp_glm_proport_t_s <- glmer(proportion_cover ~ Mean_temp + Region + (1|Year) + (1|Site), data = scaled_data, family = binomial)
+veg_temp_glm_proport_t_s_interaction <- glmer(proportion_cover ~ Mean_temp * Region + (1|Year) + (1|Site), data = scaled_data, family = binomial)
+
+
+AIC(veg_temp_glm_proport_simple, veg_temp_glm_proport_interaction, veg_temp_glm_proport_null, veg_temp_glm_proport_s, veg_temp_glm_proport_t, veg_temp_glm_proport_t_s, veg_temp_glm_proport_t_s_interaction)
+AICc(veg_temp_glm_proport_simple, veg_temp_glm_proport_interaction, veg_temp_glm_proport_null, veg_temp_glm_proport_s, veg_temp_glm_proport_t, veg_temp_glm_proport_t_s, veg_temp_glm_proport_t_s_interaction, veg_temp_glm_proport_s_random)
 
 # Plotting data ----
 (veg_temp <- ggplot(data, aes(x = Mean_temp, y = Percentage_cover)) +
